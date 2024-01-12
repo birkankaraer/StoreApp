86374273e4f78a3e11126991fe2ad4cb83047479
Birkankaraer <karaermustafabirkan@gmail.com>
2024-01-13 00:08:23 +0300

    Razor page was used, configurations were made, cart operations were done.

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -1,8 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
 
 namespace StoreApp.Pages
 {
     public class CartModel : PageModel
     {
+        private readonly IServiceManager _manager;
+		public Cart Cart { get; set; }
+
+		public CartModel(IServiceManager manager, Cart cart)
+		{
+			_manager = manager;
+			Cart = cart;
+		}
+
+        public string ReturnUrl { get; set; } = "/";
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+        }
+        public IActionResult OnPost(int productId, string returnUrl)
+        {
+            Product? product = _manager.ProductService.GetOneProduct(productId,false);
+            if (product is not null)
+            {
+                Cart.AddItem(product, 1);
+            }
+            return RedirectToPage(new { returnUrl });
+        }
+		//one by one reducing method
+        public IActionResult OnPostReduce(int productId, string returnUrl)
+        {
+			Product? product = _manager.ProductService.GetOneProduct(productId,false);
+			if (product is not null)
+            {
+				Cart.AddItem(product, -1);
+			}
+			return RedirectToPage(new { returnUrl });
+		}
+		public IActionResult OnPostRemove(int productId, string returnUrl)
+        {
+            Cart.RemoveItem(Cart.Lines.First(cl => cl.Product.ProductId.Equals(productId)).Product);
+            return RedirectToPage(new { returnUrl });
+        }
+
     }
 }
